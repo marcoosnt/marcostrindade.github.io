4411f5f81fe61354ed1fdbe3a137874edf9dd055
marcoosnt <54514287+marcoosnt@users.noreply.github.com>
2020-04-26 22:17:02 -0300

    Add files via upload

---
+++ Conversor Milhas para KM/app.js
@@ -0,0 +1,8 @@
+let numMiles = prompt("How Many Miles");
+console.log(numMiles);
+numMiles = Number (numMiles);
+let  kiloValue = numMiles* 1.60934;
+
+let message = `${numMiles} miles is equal to ${kiloValue}`;
+
+console.log(message);
